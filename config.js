ee5eada82bf6c7016609f0e40c5b4a7595dfde96
audyphan <106802109+audyphan@users.noreply.github.com>
2022-06-03 16:12:21 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hee luu babyyy!',
+    introDesc: `Anh có điều này muốn hỏi em, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'oke',
+    title: ' Anh biết em thích anh phải không phải không  😙',
+    desc: 'em không trả lời hoặc thoát ra, tức là em là vợ tớ đó nha :> ',
+    btnYes: 'yêu ơi là iuuu <33',
+    btnNo: 'anh mơ à :333',
+    question:'Nói cho tới lý do vì sao em yêu anh đii <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Tại vì anh đẹp trai hihi <33333333',
+    mess: 'Anh biết mà 🥰. Love you 300.000 😘😘',
+    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho anh đi nào.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://m.me/AudyPhan10' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
